ccdb77a03bd8bbe68c035825f56024e7b507a779
alexander-e-bauer <71612093+alexander-e-bauer@users.noreply.github.com>
2025-01-24 17:12:50 -0500

    Update

---
+++ scripts/testGoogleOauth.sh
@@ -0,0 +1,6 @@
+curl GET \
+-H "X-Heroku-Auth: apple-orange-banana-grape-mango-lemon-cherry-pear-peach-plum-kiwi-pineapple-watermelon-blueberry-strawberry-raspberry-blackberry-fig-papaya-guava-coconut-lychee-pomegranate-apricot-avocado-tangerine-lime-cantaloupe-honeydew-dragonfruit-passionfruit-durian-jackfruit-persimmon-nectarine-cranberry-gooseberry-elderberry-mulberry-boysenberry-currant-salmonberry-marionberry-huckleberry-starfruit-quince-olive-date-prune-kumquat" \
+-H "Content-Type: application/json" \
+-H "X-Real-Ip: isadora.ai" \
+-v \
+https://isadora.ai/test-oauth
