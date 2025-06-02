2011fffe535307d71a206913ce3651f3bc77886d
alexander-e-bauer <71612093+alexander-e-bauer@users.noreply.github.com>
2025-06-02 00:10:59 -0400

    Update

--- celery_app.py
+++ celery_app.py
@@ -33,6 +33,8 @@ celery_app.conf.update(
     result_serializer="json",
     timezone="UTC",
     enable_utc=True,
+    broker_use_ssl={"ssl_cert_reqs": ssl.CERT_NONE} if REDIS_URL.startswith("rediss://") else None,
+    redis_backend_use_ssl={"ssl_cert_reqs": ssl.CERT_NONE} if REDIS_URL.startswith("rediss://") else None,
 )
 
 # Add periodic task configuration
